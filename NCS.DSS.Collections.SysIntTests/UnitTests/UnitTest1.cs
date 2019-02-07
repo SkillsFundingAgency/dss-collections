738612bb70108ce7b5b73d242699cb37295303d3
Dan Hardwicke <dan.hardwicke@outlook.com>
2019-02-07 15:27:07 +0000

    Ignored 2 Unit Tests

--- NCS.DSS.Collections.SysIntTests/UnitTests/UnitTest1.cs
+++ NCS.DSS.Collections.SysIntTests/UnitTests/UnitTest1.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading;     
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using NCS.DSS.Collections.SysIntTests.Helpers;
 using NCS.DSS.Collections.SysIntTests.Models;
-using NCS.DSS.TestHelperLibrary.Helpers;
 using System.Reflection;
 
 namespace NCS.DSS.Collections.SysIntTests.UnitTests
@@ -43,21 +41,9 @@ namespace NCS.DSS.Collections.SysIntTests.UnitTests
         private EnvironmentSettings envSettings = new EnvironmentSettings();
         private readonly List<Loader> LoaderData = new List<Loader>();
 
-        [TestMethod]
-        public void DBTest()
-        {
-            SQLServerHelper sqlInstance = new SQLServerHelper();
-            sqlInstance.ToString();
 
-        }
         [TestMethod]
-        public void RegexTest()
-        {
-            Regex rxv = new Regex(@"[+-]{0,1}\d+");
-            var newValue = rxv.Match("b1").Value;
-            var value2 = System.Text.RegularExpressions.Regex.Split("234234 23 234234", @"\s+");
-        }
-        [TestMethod]
+        [Ignore]
         public void TimeTest()
         {
             var a = DateTime.Now.Ticks;
@@ -69,139 +55,140 @@ namespace NCS.DSS.Collections.SysIntTests.UnitTests
 
         }
 
-        //[TestMethod]
-        //public void LoadCustomers()
-        //{
+        [TestMethod]
+        [Ignore]
+        public void LoadCustomers()
+        {
             
-        //    // open json file
-        //    string fileName = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + $@"\json\test_customers.json";
-        //    using (var sr = new StreamReader(fileName))
-        //    {
-        //        // load into collection
-        //        var serializer = new JsonSerializer();
-        //        using (var reader = new JsonTextReader(sr))
-        //        {
-        //            var customers = serializer.Deserialize<LoadCustomer[]>(reader);
-        //            //var loaderItem = JsonConvert.DeserializeObject(Loader[] > (reader);
+            // open json file
+            string fileName = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + $@"\json\test_customers.json";
+            using (var sr = new StreamReader(fileName))
+            {
+                // load into collection
+                var serializer = new JsonSerializer();
+                using (var reader = new JsonTextReader(sr))
+                {
+                    var customers = serializer.Deserialize<LoadCustomer[]>(reader);
+                    //var loaderItem = JsonConvert.DeserializeObject(Loader[] > (reader);
 
-        //           /* JsonSerializerSettings settings = new JsonSerializerSettings
-        //            {
-        //                ContractResolver = new ShouldSerializeContractResolver()
-        //            };*/
+                   /* JsonSerializerSettings settings = new JsonSerializerSettings
+                    {
+                        ContractResolver = new ShouldSerializeContractResolver()
+                    };*/
 
-        //            // loop through collection
-        //            foreach ( var customer in customers )
-        //            {
-        //                // submit customer
-        //                string json = JsonConvert.SerializeObject(customer, Formatting.Indented);//, settings);
+                    // loop through collection
+                    foreach ( var customer in customers )
+                    {
+                        // submit customer
+                        string json = JsonConvert.SerializeObject(customer, Formatting.Indented);//, settings);
 
-        //                // don't want to send internal reference
-        //                json = JsonHelper.RemovePropertyFromJsonString(json, "LoaderRef");
+                        // don't want to send internal reference
+                        json = JsonHelper.RemovePropertyFromJsonString(json, "LoaderRef");
 
-        //                var response = RestHelper.Post(envSettings.BaseUrl + "/customers/api/customers/", json, envSettings.TouchPointId, envSettings.SubscriptionKey);
+                        var response = RestHelper.Post(envSettings.BaseUrl + "/customers/api/customers/", json, envSettings.TouchPointId, envSettings.SubscriptionKey);
 
-        //                if (response.StatusCode == System.Net.HttpStatusCode.Created)
-        //                {
-        //                    // store returned customer ID with loader reference
-        //                   // // // LoaderData.Add(new Loader(customer.LoaderRef);//, JsonHelper.GetPropertyFromJsonString(response.Content,"CustomerId")));
-        //                }
+                        if (response.StatusCode == System.Net.HttpStatusCode.Created)
+                        {
+                            // store returned customer ID with loader reference
+                            LoaderData.Add(new Loader(customer.LoaderRef, JsonHelper.GetPropertyFromJsonString(response.Content,"CustomerId")));
+                        }
 
-        //            }
-        //        }
-        //    }
-
-
-        //    // now load address file
-        //    fileName = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + $@"\json\test_addresses.json";
-        //    using (var sr = new StreamReader(fileName))
-        //    {
-        //        // load into collection
-        //        var serializer = new JsonSerializer();
-        //        using (var reader = new JsonTextReader(sr))
-        //        {
-        //            var addresses = serializer.Deserialize<LoadAddress[]>(reader);
-        //            //var loaderItem = JsonConvert.DeserializeObject(Loader[] > (reader);
-
-        //            /* JsonSerializerSettings settings = new JsonSerializerSettings
-        //             {
-        //                 ContractResolver = new ShouldSerializeContractResolver()
-        //             };*/
+                    }
+                }
+            }
 
 
+            // now load address file
+            fileName = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + $@"\json\test_addresses.json";
+            using (var sr = new StreamReader(fileName))
+            {
+                // load into collection
+                var serializer = new JsonSerializer();
+                using (var reader = new JsonTextReader(sr))
+                {
+                    var addresses = serializer.Deserialize<LoadAddress[]>(reader);
+                    //var loaderItem = JsonConvert.DeserializeObject(Loader[] > (reader);
 
-        //            // loop through collection
-        //            foreach (var address in addresses)
-        //            {
-        //                // can we find the loader ref in the loaderdata
-
-        //                var list = LoaderData.Where(item => item.LoaderReference == address.LoaderRef);
-        //                foreach (var item in list)
-        //                {
-        //    //                address.CustomerId = item.CustomerID;
-
-        //                    string json = JsonConvert.SerializeObject(address, Formatting.Indented);//, settings);
-
-        //                    // don't want to send internal reference
-        //                    json = JsonHelper.RemovePropertyFromJsonString(json, "LoaderRef");
-
-        //                    var response = RestHelper.Post(envSettings.BaseUrl + "addresses/api/Customers/" + address.CustomerId + "/addresses/", json, envSettings.TouchPointId, envSettings.SubscriptionKey);
-
-        //                    /* if (response.StatusCode == System.Net.HttpStatusCode.Created)
-        //                     {
-        //                         // store returned customer ID with loader reference
-        //                         LoaderData.Add(new Loader(address.LoaderRef, GetPropertyFromJsonString(response.Content, "CustomerId")));
-        //                     }*/
-        //                }
-
-        //            }
-        //        }
-        //    }
-        //    // now load contact file
-        //    fileName = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + $@"\json\test_contacts.json";
-        //    using (var sr = new StreamReader(fileName))
-        //    {
-        //        // load into collection
-        //        var serializer = new JsonSerializer();
-        //        using (var reader = new JsonTextReader(sr))
-        //        {
-        //            var contacts = serializer.Deserialize<LoadContact[]>(reader);
-        //            //var loaderItem = JsonConvert.DeserializeObject(Loader[] > (reader);
-
-        //            /* JsonSerializerSettings settings = new JsonSerializerSettings
-        //                {
-        //                    ContractResolver = new ShouldSerializeContractResolver()
-        //                };*/
+                    /* JsonSerializerSettings settings = new JsonSerializerSettings
+                     {
+                         ContractResolver = new ShouldSerializeContractResolver()
+                     };*/
 
 
 
-        //            // loop through collection
-        //            foreach (var contact in contacts)
-        //            {
-        //                // can we find the loader ref in the loaderdata
+                    // loop through collection
+                    foreach (var address in addresses)
+                    {
+                        // can we find the loader ref in the loaderdata
 
-        //                var list = LoaderData.Where(item => item.LoaderReference == contact.LoaderRef);
-        //                foreach (var item in list)
-        //                {
-        //       //             contact.CustomerId = item.CustomerID;
+                        var list = LoaderData.Where(item => item.LoaderReference == address.LoaderRef);
+                        foreach (var item in list)
+                        {
+                            address.CustomerId = item.CustomerID;
 
-        //                    string json = JsonConvert.SerializeObject(contact, Formatting.Indented);//, settings);
+                            string json = JsonConvert.SerializeObject(address, Formatting.Indented);//, settings);
 
-        //                    // don't want to send internal reference
-        //                    json = JsonHelper.RemovePropertyFromJsonString(json, "LoaderRef");
+                            // don't want to send internal reference
+                            json = JsonHelper.RemovePropertyFromJsonString(json, "LoaderRef");
 
-        //                    var response = RestHelper.Post(envSettings.BaseUrl + "contacts/api/Customers/" + contact.CustomerId + "/contacts/", json, envSettings.TouchPointId, envSettings.SubscriptionKey);
+                            var response = RestHelper.Post(envSettings.BaseUrl + "addresses/api/Customers/" + address.CustomerId + "/addresses/", json, envSettings.TouchPointId, envSettings.SubscriptionKey);
 
-        //                    /* if (response.StatusCode == System.Net.HttpStatusCode.Created)
-        //                        {
-        //                            // store returned customer ID with loader reference
-        //                            LoaderData.Add(new Loader(address.LoaderRef, GetPropertyFromJsonString(response.Content, "CustomerId")));
-        //                        }*/
-        //                }
+                            /* if (response.StatusCode == System.Net.HttpStatusCode.Created)
+                             {
+                                 // store returned customer ID with loader reference
+                                 LoaderData.Add(new Loader(address.LoaderRef, GetPropertyFromJsonString(response.Content, "CustomerId")));
+                             }*/
+                        }
 
-        //            }
-        //        }
-        //    }
+                    }
+                }
+            }
+            // now load contact file
+            fileName = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + $@"\json\test_contacts.json";
+            using (var sr = new StreamReader(fileName))
+            {
+                // load into collection
+                var serializer = new JsonSerializer();
+                using (var reader = new JsonTextReader(sr))
+                {
+                    var contacts = serializer.Deserialize<LoadContact[]>(reader);
+                    //var loaderItem = JsonConvert.DeserializeObject(Loader[] > (reader);
+
+                    /* JsonSerializerSettings settings = new JsonSerializerSettings
+                        {
+                            ContractResolver = new ShouldSerializeContractResolver()
+                        };*/
+
+
+
+                    // loop through collection
+                    foreach (var contact in contacts)
+                    {
+                        // can we find the loader ref in the loaderdata
+
+                        var list = LoaderData.Where(item => item.LoaderReference == contact.LoaderRef);
+                        foreach (var item in list)
+                        {
+                            contact.CustomerId = item.CustomerID;
+
+                            string json = JsonConvert.SerializeObject(contact, Formatting.Indented);//, settings);
+
+                            // don't want to send internal reference
+                            json = JsonHelper.RemovePropertyFromJsonString(json, "LoaderRef");
+
+                            var response = RestHelper.Post(envSettings.BaseUrl + "contacts/api/Customers/" + contact.CustomerId + "/contacts/", json, envSettings.TouchPointId, envSettings.SubscriptionKey);
+
+                            /* if (response.StatusCode == System.Net.HttpStatusCode.Created)
+                                {
+                                    // store returned customer ID with loader reference
+                                    LoaderData.Add(new Loader(address.LoaderRef, GetPropertyFromJsonString(response.Content, "CustomerId")));
+                                }*/
+                        }
+
+                    }
+                }
+            }
                 
-        //}
+        }
     }
 }
